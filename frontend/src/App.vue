6341bea43cae57bb443b6725fe0cd9802ed2d71c
Alexander Amin <alex@alexamin.de>
2025-05-31 15:09:31 -0400

    Added base vue project

---
+++ frontend/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <span class="mx auto px-3 rounded-lg bg-primary text-white my-8">Hello Dr. Llamahattan</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
